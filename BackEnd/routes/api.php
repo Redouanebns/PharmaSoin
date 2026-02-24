bd68c9b99d48a30f49d69b0e75907a61356b4d3d
Abd Errahmane Meqdad <abderahmanmekdad1@gmail.com>
2026-02-24 16:54:27 +0000

    Revert "ajout page about frontend et backend"
    This reverts commit 33d9ec0012ecb1594e32076e0e85339256a3300e.

--- BackEnd/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EquipeController;
-use App\Http\Controllers\ServiceController;
-
-Route::get('/equipes', [EquipeController::class, 'index']);
-Route::get('/services', [ServiceController::class, 'index']);
